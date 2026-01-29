ff648e83715dc2c53b0b68ee30f48a67b7e20d06
sevcenkoevgen56-design <sevcenkoevgen56@gmail.com>
2026-01-29 22:43:09 +0200

    Add files via upload

---
+++ diary.js
@@ -0,0 +1,11 @@
+// Простий менеджер щоденника
+class DiaryManager {
+    constructor() {
+        console.log('DiaryManager ініціалізовано');
+    }
+    
+    // Цей файл тепер інтегрований в основний script.js
+    // Можна видалити або залишити для майбутнього розширення
+}
+
+window.diaryManager = new DiaryManager();
